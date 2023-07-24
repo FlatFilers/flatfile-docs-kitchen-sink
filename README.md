1d91aeac5f15f907e11eef03052db326b9ee1783
Ashley Mulligan <ashleygmulligan@gmail.com>
2023-07-24 14:06:46 -0400

    feat: initial commit

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # flatfile-docs-kitchen-sink
-This repo includes all of the examples from the flatfile docs
+
+This repo includes all of the examples from the [Flatfile docs](https://flatfile.com/docs/).
+
+## Setup Local Environment Variables
+
+Rename `.env.example` to `.env` and update to your creds. This file should look like:
+
+- FLATFILE_ENVIRONMENT_ID=us_env_1234
+- FLATFILE_API_KEY=sk_1234
+- WEBHOOK_SITE_URL=https://webhook.site/1234
+
+## Developing
+
+If using javascript, run `npx flatfile develop javascript/<path-to-file>.js` to start a local listener.
+
+If using typescript, run `npx flatfile develop typescript/<path-to-file>.ts` to start a local listener.
+
+## Deploying
+
+Use `npx flatfile deploy <file name>` to deploy your listener to Flatfile.
+
+## See all code examples
+
+To see all of the code examples from the docs, head to the [flatfile-docs-kitchen-sink](https://github.com/FlatFilers/flatfile-docs-kitchen-sink) repo.
---
+++ javascript/index.js
@@ -0,0 +1,100 @@
+/**
+ * Get your Secret key at: https://platform.flatfile.com/developers and then
+ * paste it in Tools > Secrets > FLATFILE_API_KEY.
+ */
+
+import { recordHook } from "@flatfile/plugin-record-hook";
+import api from "@flatfile/api";
+import axios from "axios";
+
+/**
+ * Write a basic Flatfile event subscriber. You can do nearly anything
+ * that reacts to events inside Flatfile. To start - Click Run
+ */
+
+export default function flatfileEventListener(listener) {
+  listener.on("**", ({ topic }) => {
+    console.log(`Received event: ${topic}`);
+  });
+
+  listener.use(
+    recordHook("contacts", (record) => {
+      const value = record.get("firstName");
+      if (typeof value === "string") {
+        record.set("firstName", value.toLowerCase());
+      }
+
+      const email = record.get("email");
+      const validEmailAddress = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      if (!validEmailAddress.test(email)) {
+        console.log("Invalid email address");
+        record.addError("email", "Invalid email address");
+      }
+
+      return record;
+    })
+  );
+
+  listener.filter({ job: "workbook:submitAction" }, (configure) => {
+    configure.on("job:ready", async (event) => {
+      const { jobId, workbookId } = event.context;
+
+      const sheets = await api.sheets.list({ workbookId });
+
+      const records = {};
+      for (const [index, element] of sheets.data.entries()) {
+        records[`Sheet[${index}]`] = await api.records.get(element.id);
+      }
+
+      try {
+        await api.jobs.ack(jobId, {
+          info: "Starting job to submit action to webhook.site",
+          progress: 10,
+        });
+
+        const webhookReceiver =
+          process.env.WEBHOOK_SITE_URL ||
+          "https://webhook.site/c83648d4-bf0c-4bb1-acb7-9c170dad4388"; //update this
+
+        const response = await axios.post(
+          webhookReceiver,
+          {
+            ...event.payload,
+            method: "axios",
+            sheets,
+            records,
+          },
+          {
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+
+        if (response.status === 200) {
+          await api.jobs.complete(jobId, {
+            outcome: {
+              message:
+                "Data was successfully submitted to webhook.site. Go check it out at " +
+                webhookReceiver +
+                ".",
+            },
+          });
+        } else {
+          throw new Error("Failed to submit data to webhook.site");
+        }
+      } catch (error) {
+        console.log(`webhook.site[error]: ${JSON.stringify(error, null, 2)}`);
+
+        await api.jobs.fail(jobId, {
+          outcome: {
+            message:
+              "This job failed probably because it couldn't find the webhook.site URL.",
+          },
+        });
+      }
+    });
+  });
+}
+
+// You can see the full example used in our getting started guide in ./full-example.js
